fc444bd1e986089dbad6e25761252f5d8e918ffa
Mohamed Mosaad <mohamed.mosaad@elmenus.com>
2024-06-23 16:40:35 +0300

    [INITIAL Commit] Merge two sorted arrays

---
+++ tests/test_merge_sorted_array.py
@@ -0,0 +1,20 @@
+import unittest
+import pytest
+
+from py_src import merge_sorted_array  
+
+@pytest.mark.parametrize("nums1, nums2, m, n, expected_output", 
+                         [
+                            ([0], [1], 0, 1, [1]),
+                            # (-1, 1, 0),
+                            # (-1, -1, -2),
+                            # (0, 0, 0)
+                         ]
+)
+
+# class Test_merge_sorted_array(unittest.TestCase):
+def test_merge_empty_array(nums1, nums2, m, n, expected_output):
+    assert merge_sorted_array.Solution.merge_sorted_arrays(nums1, nums2, m, n) == expected_output
+
+# if __name__ == "__main__":
+#     unittest.main()
